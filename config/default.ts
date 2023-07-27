5b2c8d0e86398a644850cf2280bd761914940b06
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-27 17:20:07 -0300

    Test_5

--- config/default.ts
+++ config/default.ts
@@ -4,5 +4,5 @@ const dbUser:any =  process.env.DB_USER
 export default {
   port: 3000,
   dbUrl: `mongodb+srv://${dbUser}:${dbPassword}@restfulapi.0kzidwr.mongodb.net/?retryWrites=true&w=majority`,
-  env: 'development'
+  env: 'production'
 };
